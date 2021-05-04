cc1693eb1ca890c2e64afdc0627a8c71f536aff2
yuangan <411246903@qq.com>
2021-05-04 15:02:28 +0800

    xla_device

--- basicsr/models/base_model.py
+++ basicsr/models/base_model.py
@@ -8,6 +8,8 @@ from torch.nn.parallel import DataParallel, DistributedDataParallel
 from basicsr.models import lr_scheduler as lr_scheduler
 from basicsr.utils.dist_util import master_only
 
+from torch_xla.core.xlad_model as xm
+
 logger = logging.getLogger('basicsr')
 
 
@@ -16,7 +18,8 @@ class BaseModel():
 
     def __init__(self, opt):
         self.opt = opt
-        self.device = torch.device('cuda' if opt['num_gpu'] != 0 else 'cpu')
+#         self.device = torch.device('cuda' if opt['num_gpu'] != 0 else 'cpu')
+        self.device = xm.xla_device()
         self.is_train = opt['is_train']
         self.schedulers = []
         self.optimizers = []
