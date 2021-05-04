cda78b082fc08b5704517f4262b6c6b27085dd69
yuangan <411246903@qq.com>
2021-05-04 15:39:28 +0800

    Update video_base_model.py

--- basicsr/models/video_base_model.py
+++ basicsr/models/video_base_model.py
@@ -32,8 +32,8 @@ class VideoBaseModel(SRModel):
                 self.metric_results[folder] = torch.zeros(
                     num_frame,
                     len(self.opt['val']['metrics'])).to(xm.xla_device())
-                print(self.metric_results[folder])
-        print('gg')
+                #print(self.metric_results[folder])
+        #print('gg')
         rank, world_size = get_dist_info()
         if with_metrics:
             for _, tensor in self.metric_results.items():
